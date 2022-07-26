853c6242a1cb8b4a4ba7187cc72dece76b833bce
MohdAnas07 <anas98144@gmail.com>
2022-07-26 22:06:34 +0530

    signup-login API complete

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express();
 require('./db/config')
+
 const User = require('./db/User');
 const cors = require('cors');
 
@@ -11,7 +12,22 @@ app.use(express.json());
 app.post('/register', async (req, res) => {
     let user = new User(req.body);
     let result = await user.save()
+    result = result.toObject();
+    delete result.password;
     res.send(result);
+})
+
+app.post('/login', async (req, res) => {
+    if (req.body.password && req.body.email) {
+        let user = await User.findOne(req.body).select('-password')
+        if (user) {
+            res.send(user);
+        } else {
+            res.send({ result: 'No user found' });
+        }
+    } else {
+        res.send({ result: 'No user found' });
+    }
 })
 
 
