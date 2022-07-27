17d47434ddf8d445e62106ea2d2f5172beef2a39
MohdAnas07 <anas98144@gmail.com>
2022-07-27 12:41:37 +0530

    delete API Complete

--- index.js
+++ index.js
@@ -50,6 +50,11 @@ app.get('/products', async (req, res) => {
 })
 
 
+app.delete('/product/:id', async (req, res) => {
+    const result = await Product.deleteOne({ _id: req.params.id });
+    res.send(result)
+})
+
 
 
 
